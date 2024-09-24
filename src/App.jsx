024f5c74749c2e3647781a4c2bc2e02c303ce10a
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-24 15:54:47 -0600

    Add color divs, selectedColor, and selected border

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import { useState } from "react";
 // Write your Color component here
 
+const Color = (props) => {
+  const { color, selectedColor, setSelectedColor } = props;
+  const className = color === selectedColor ? `${color} selected` : color;
+  return (
+    <div
+      className={className}
+      onClick={() => props.setSelectedColor(props.color)}
+    ></div>
+  );
+};
+
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color
+          color="red"
+          selectedColor={selectedColor}
+          setSelectedColor={setSelectedColor}
+        />
+        <Color
+          color="blue"
+          selectedColor={selectedColor}
+          setSelectedColor={setSelectedColor}
+        />
+        <Color
+          color="orange"
+          selectedColor={selectedColor}
+          setSelectedColor={setSelectedColor}
+        />
+      </div>
     </div>
   );
 };
